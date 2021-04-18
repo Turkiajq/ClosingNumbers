304f0998a06eec9f65cf3b9e7451135e87a33b1a
turki-j <62262729+turki-j@users.noreply.github.com>
2021-04-19 01:12:59 +0300

    implemented Closing numbers logic

---
+++ ClosingNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClosingNumbers
+{
+    class MainClass
+    {
+        public static bool isClosingNum(string setOfNums)
+        {
+            if (setOfNums.Length % 2 != 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < setOfNums.Length; i++)
+            {
+                for (int j = setOfNums.Length-1; j > 0; j--)
+                {
+                    
+                    if (setOfNums[i] != setOfNums[j])
+                    {
+                        return false;
+                    }
+                    i++;
+                }
+            }
+
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(isClosingNum("654321123456"));
+        }
+    }
+}
